055343f64db64da8b10926ccfd2e863f1b3a9272
ajeer333 <ajeer333@gmail.com>
2023-03-21 15:35:50 +0100

    commit

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -1,34 +1,54 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
+const experiencesSchema = new Schema(
+  {
+    role: { type: String },
+    company: { type: String },
+    startDate: { type: String },
+    endDate: { type: String }, //could be null
+    description: { type: String },
+    area: { type: String },
+    image: {
+      default: "http://placekitten.com/200/300",
+      type: String,
+    },
+    //users: [usersSchema]
+  },
+  { timestamps: true }
+);
 
 const validateEmail = function (email) {
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
 };
 
 const usersSchema = new Schema(
-    {
-        name: { type: String, required: true },
-        surname: { type: String, required: true },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-            // unique: true,
-            required: 'Email address is required',
-            validate: [validateEmail, 'Please fill a valid email address'],
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-        },
-        bio: { type: String },
-        title: { type: String },
-        area: { type: String },
-        image: { type: String },
+  {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      // unique: true,
+      required: "Email address is required",
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
-    {
-        timestamps: true,
-    }
-)
+    bio: { type: String },
+    title: { type: String },
+    area: { type: String },
+    image: { type: String },
+    experiences: { type: [experiencesSchema] },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default model("User", usersSchema) 
+export default model("User", usersSchema);
