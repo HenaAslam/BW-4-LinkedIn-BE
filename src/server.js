e83960f984f48e48c5f6603f589b232ea38b2ffd
ajeer333 <ajeer333@gmail.com>
2023-03-23 16:41:37 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ import Express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import {
-    badRequestHandler,
-    notFoundHandler,
-    genericErrorHandler,
+  badRequestHandler,
+  notFoundHandler,
+  genericErrorHandler,
 } from "./errorHandlers.js";
 import postRouter from "./api/posts/index.js";
 import usersRouter from "./api/users/index.js";
@@ -22,20 +22,20 @@ server.use(Express.static(publicFolderPath));
 // **************************************** MIDDLEWARES *****************************************
 const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
 server.use(
-    cors({
-        origin: (currentOrigin, corsNext) => {
-            if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
-                corsNext(null, true);
-            } else {
-                corsNext(
-                    createHttpError(
-                        400,
-                        `Origin ${currentOrigin} is not in the whitelist!`
-                    )
-                );
-            }
-        },
-    })
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        corsNext(null, true);
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
 );
 server.use(Express.json());
 
@@ -53,9 +53,9 @@ server.use(genericErrorHandler);
 mongoose.connect(process.env.MONGO_URL);
 
 mongoose.connection.on("connected", () => {
-    console.log("✅ Successfully connected to Mongo!");
-    server.listen(port, () => {
-        console.table(listEndpoints(server));
-        console.log(`✅ Server is running on port ${port}`);
-    });
+  console.log("✅ Successfully connected to Mongo!");
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`✅ Server is running on port ${port}`);
+  });
 });
